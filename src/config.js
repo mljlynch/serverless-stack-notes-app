08ef0b75f3374c2fc195684200ba7807c747b392
aharabiosciences <jacoblync@gmail.com>
2021-02-07 10:35:36 -0500

    Adding a redirects file

--- src/config.js
+++ src/config.js
@@ -1,18 +1,21 @@
 const config = {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_51IHz9KDO2dStaKVFqSP8QJXFOhXbIk10sWkLBQ6Tx8OlVDDQQ5OdhJiBmHBttBU4xsn5Dvt15FW0pyRLbtFlasSy00K3OdjEUl",
     s3: {
       REGION: "us-east-1",
       BUCKET: "fig-notes-app-uploads",
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://2p2a7c38vl.execute-api.us-east-1.amazonaws.com/prod/",
+      URL: "https://2p2a7c38vl.execute-api.us-east-1.amazonaws.com/prod",
     },
     cognito: {
       REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_hnB2YP6Qx",
       APP_CLIENT_ID: "3c0oevrbtvvmo29smj3dl3nad3",
-      IDENTITY_POOL_ID: "us-east-1:6c38fd26-776b-4c53-9cbf-12feeda0f0b",
+      IDENTITY_POOL_ID: "us-east-1:6c38fd26-776b-4c53-9cbf-12feeda0f0be",
     },
   };
+
   
   export default config;
