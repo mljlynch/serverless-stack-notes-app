9ad5c943ec21d805895de87972915f502b188db0
aharabiosciences <jacoblync@gmail.com>
2021-02-07 15:26:22 -0500

    Adding error reporting

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Nav from "react-bootstrap/Nav";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import { onError } from "./libs/errorLib";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
@@ -70,9 +71,11 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-          <Routes />
-        </AppContext.Provider>
+        <ErrorBoundary>
+  <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+    <Routes />
+  </AppContext.Provider>
+</ErrorBoundary>
       </div>
     )
 );
